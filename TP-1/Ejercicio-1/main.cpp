1600131e40a56f4322fd5f27b5344a98c6f066d7
Brian Teper <brian.teper@outlook.com>
2012-08-18 19:07:21 -0300

    Ejercicio-1 del TP-1

--- TP-1/Ejercicio-1/main.cpp
+++ TP-1/Ejercicio-1/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 using namespace std;
 
+#define MAX 31
+#define MENSAJE "Ingrese un caracter, para finalizar ingrese 0:"
+
 void main(){
+	char *pvec = new char[MAX];
+	int cant = 0;
 	
+	cout << MENSAJE << endl;
+	cin >> *pvec;
+
+	while (*pvec != '0'){
+		cant++;
+		*pvec++;
+		cout << MENSAJE << endl;
+		cin >> *pvec;
+	}
+
+	*pvec = '\0';
+	
+	for (int i = 0; i < cant; i++){
+		pvec--;
+		cout << *pvec << endl; 
+	}
+
+	delete [] pvec;
 }
