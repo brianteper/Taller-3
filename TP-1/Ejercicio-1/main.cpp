ec3cf61f2f9c660a3867265a475105c9e46da884
Brian Teper <brian.teper@outlook.com>
2012-08-25 21:31:44 -0300

    Modificación para tomar una cadena de caracteres.

--- TP-1/Ejercicio-1/main.cpp
+++ TP-1/Ejercicio-1/main.cpp
@@ -2,45 +2,53 @@
 using namespace std;
 
 #define MAX 30
-#define MSGINGRESO "Ingrese un caracter, para finalizar ingrese 0:"
+#define MSGINGRESO "Ingrese una cadena de caracteres:"
 #define MSGSHOW "La cadena al reves es:"
 
-void ingreso(char *&puntero, int &cantidad);
+void ingreso(char *&puntero);
+int cantcaracteres(char *&puntero);
 void mostrar(char *&puntero, int cantidad);
 void liberar(char *&puntero);
 
 void main(){
 	char *pvec = new char[MAX+1];
-	int cant = 0;
 
-	ingreso(pvec, cant);
+	ingreso(pvec);
 	
+	int cant = cantcaracteres(pvec);
+
 	mostrar(pvec, cant);
 
 	liberar(pvec);
 }
 
-void ingreso(char *&puntero, int &cantidad){
+void ingreso(char *&puntero){
 	cout << MSGINGRESO << endl;
-	cin >> *puntero;
+	//Obtengo toda la línea incluyendo los espacios, agrego 1 a MAX para \0
+	cin.getline(puntero, MAX+1);
+}
 
-	while (*puntero != '0' && cantidad < MAX){
+int cantcaracteres(char *&puntero){
+	int cantidad = 0;
+	
+	//Cuento la cantidad de caracteres como strlen y me ubico en la última posición para luego mostrar al revés
+	while (*puntero != '\0'){
 		cantidad++;
 		*puntero++;
-		cout << MSGINGRESO << endl;
-		cin >> *puntero;
 	}
 
-	*puntero = '\0';
+	return cantidad;
 }
 
 void mostrar(char *&puntero, int cantidad){
 	system("cls");
 	cout << MSGSHOW << endl;
 
+	//Muestro al reves la cantidad de caracteres ingresados
 	for (int i = 0; i < cantidad; i++){
+		//Comienzo retrocediendo el puntero porque el ultimo caracter es \0
 		puntero--;
-		cout << *puntero << endl; 
+		cout << *puntero; 
 	}	
 }
 
